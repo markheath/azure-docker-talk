946806ad6e6878a5e1fb485b21655b17b32bd4c0
Mark Heath <mark.heath@gmail.com>
2018-02-27 11:33:16 +0000

    adding dns name label to ACI container group demo

--- aci-groups.ps1
+++ aci-groups.ps1
@@ -2,6 +2,8 @@
 # containers in a single container group and get them talking
 # to get it working had to make port 3306 public and use 127.0.0.1 as database address
 
+# https://docs.microsoft.com/en-gb/azure/templates/microsoft.containerinstance/containergroups
+
 az account show --query name -o tsv
 az account set -s "MVP"
 
@@ -10,17 +12,19 @@ $location="westeurope"
 az group create -n $resourceGroup -l $location
 
 $containerGroupName = "myWordpress"
+$dnsNameLabel = "wordpressaci"
 
 az group deployment create `
     -n TestDeployment -g $resourceGroup `
     --template-file "aci-wordpress.json" `
     --parameters 'mysqlPassword=My5q1P@s5w0rd!' `
-    --parameters "containerGroupName=$containerGroupName"
+    --parameters "containerGroupName=$containerGroupName" `
+    --parameters "dnsNameLabel=$dnsNameLabel"
 
 az container list -g $resourceGroup -o table
 
 az container show -g $resourceGroup -n $containerGroupName `
-        --query ipAddress.ip -o tsv
+        --query ipAddress.fqdn -o tsv
 
 az container logs -g $resourceGroup -n $containerGroupName
 
