faa109c6735a67c382a6acc14532cf7a75fda905
Mark Heath <mark.heath@gmail.com>
2018-12-10 21:29:16 +0000

    updating the AKS demo

--- azurethamesvalley/demo4-aks.ps1
+++ azurethamesvalley/demo4-aks.ps1
@@ -3,18 +3,18 @@ az account show --query name -o tsv
 az account set -s "MVP"
 
 # 1. Create a resource group
-$resourceGroup = "AKSThamesValleyDemo"
+$aksrg = "AKSThamesValleyDemo"
 $location = "westeurope" # see valid regions at https://azure.microsoft.com/en-gb/global-infrastructure/services/?products=kubernetes-service
-az group create -n $resourceGroup -l $location
+az group create -n $aksrg -l $location
 
 # Create our AKS Cluster (takes about 8 minutes)
 $clusterName = "MarkAks"
-az aks create -g $resourceGroup -n $clusterName --node-count 3 --generate-ssh-keys
+az aks create -g $aksrg -n $clusterName --node-count 3 --generate-ssh-keys
 
 # Get credentials for kubectl to use
-az aks get-credentials -g $resourceGroup -n $clusterName
+az aks get-credentials -g $aksrg -n $clusterName
 
-# 6. Check we're connected
+# Check we're connected
 kubectl get nodes
 
 # deploy the example vote app
@@ -24,7 +24,7 @@ kubectl apply -f .\aks-vote.yml
 kubectl get service --watch
 
 # run kubernetes dashboard
-az aks browse -g $resourceGroup -n $clusterName
+az aks browse -g $aksrg -n $clusterName
 
 # n.b. if the dashboard shows errors, you may need this fix:
 # https://pascalnaber.wordpress.com/2018/06/17/access-dashboard-on-aks-with-rbac-enabled/
@@ -37,4 +37,4 @@ kubectl apply -f .\aks-vote-v2.yml
 kubectl delete -f .\aks-vote-v2.yml
 
 # Clean up
-az group delete -n $resourceGroup --yes --no-wait
+az group delete -n $aksrg --yes --no-wait
