338fa17eec5001546e31c693a85b42d484851df7
Mark Heath <mark.heath@gmail.com>
2019-02-04 17:38:04 +0000

    updating service names

--- azurethamesvalley/demo3-sfmesh.ps1
+++ azurethamesvalley/demo3-sfmesh.ps1
@@ -58,8 +58,8 @@ az mesh network show -g $resGroup -n "votingNetwork"
 az mesh service list -g $resGroup --app-name $appName -o table
 
 # explore the services
-az mesh service show -g $resGroup --app-name $appName -n "resultService"
-az mesh service show -g $resGroup --app-name $appName -n "voteService"
+az mesh service show -g $resGroup --app-name $appName -n "result-service"
+az mesh service show -g $resGroup --app-name $appName -n "vote-service"
 
 # get public ip address
 $publicIp = az mesh gateway show -n "ingressGateway" -g $resGroup --query "ipAddress" -o tsv
@@ -69,10 +69,10 @@ Start-Process http://$($publicIp):8080/ # voting
 Start-Process http://$($publicIp):8081/ # results - not working
 
 # view logs for containers
-az mesh code-package-log get -g $resGroup --application-name $appName --service-name "voteService" --replica-name 0 --code-package-name "voteCode"
-az mesh code-package-log get -g $resGroup --application-name $appName --service-name "workerService" --replica-name 0 --code-package-name "workerCode"
-az mesh code-package-log get -g $resGroup --application-name $appName --service-name "resultService" --replica-name 0 --code-package-name "resultCode"
-az mesh code-package-log get -g $resGroup --application-name $appName --service-name "db" --replica-name 0 --code-package-name "tidbCode"
+az mesh code-package-log get -g $resGroup --application-name $appName --service-name "vote-service" --replica-name 0 --code-package-name "voteCode"
+az mesh code-package-log get -g $resGroup --application-name $appName --service-name "worker-service" --replica-name 0 --code-package-name "workerCode"
+az mesh code-package-log get -g $resGroup --application-name $appName --service-name "result-service" --replica-name 0 --code-package-name "resultCode"
+az mesh code-package-log get -g $resGroup --application-name $appName --service-name "db-service" --replica-name 0 --code-package-name "tidbCode"
 
 # scale up vote container to 3 instances (currently seems unreliable)
 # https://github.com/Azure/service-fabric-mesh-preview/issues/266
@@ -83,13 +83,13 @@ az mesh deployment create -g $resGroup --template-file $templateFile `
 az mesh service list -g $resGroup --app-name $appName -o table
 
 # explore the vote service
-az mesh service show -g $resGroup --app-name $appName -n "voteService" -o table
+az mesh service show -g $resGroup --app-name $appName -n "vote-service" -o table
 
 # see the replicas
-az mesh service-replica list -g $resGroup --app-name $appName --service-name "voteService" -o table
+az mesh service-replica list -g $resGroup --app-name $appName --service-name "vote-service" -o table
 
 # explore a particular replica
-az mesh service-replica show -g $resGroup --app-name $appName --service-name "voteService" -n 0
+az mesh service-replica show -g $resGroup --app-name $appName --service-name "vote-service" -n 0
 
 # delete everything
 az group delete -n $resGroup -y
